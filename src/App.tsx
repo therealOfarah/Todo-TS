df2b889dfcac75292b1ea79768816797b3ff6441
Omar Farah <omaarfaarah2019@gmail.com>
2022-10-27 17:22:39 -0400

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { ITask } from './interFace';
 import './App.css';
 
 function App() {
+  // defining the type of states
+  const[task, setTask]= useState<string>("")
+  const[deadline, setDeadline]= useState<number>(0)
+  const[todo, setTodo]= useState<ITask[]>([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void =>{
+    if( event.target.name === 'task'){
+      setTask(event.target.value)
+    }else{
+      setDeadline(Number(event.target.value))
+    }
+  }
+  const addTask = () =>{
+    const newTask ={task: task,  deadline: deadline}
+    setTodo([...todo, newTask])
+    console.log(todo)
+  }
   return (
     <div className="App">
       <div className='header'>
         <div className="inputContainer">
-        <input type="text" placeholder='Task name' />
-        <input type="text" placeholder='Due by' />
+        <input type="text" placeholder='Task name' name='task' onChange={handleChange} />
+        <input type="number" name='deadline' placeholder='Due by' onChange={handleChange} />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
-      <div className='todo'>
+      <div className='todo' >
       </div>
     </div>
   );
